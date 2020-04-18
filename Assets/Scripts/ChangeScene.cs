3d41e471506da9a76ce200ffb49344d430e94fcd
Chris Creighton (@creighcl) <creighcl6510@gmail.com>
2020-04-17 20:14:27 -0400

    skeleton stuff

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void SwapToScene(string sceneName) {
+        Debug.Log(sceneName);
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
---
+++ Assets/Scripts/SomeBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start Happened!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("UPDATE SPAM GO");
+    }
+}
