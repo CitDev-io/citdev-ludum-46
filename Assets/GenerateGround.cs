cfd429d199db0a2711e1bf52c94ae0a80327f848
Chris Creighton (@creighcl) <creighcl6510@gmail.com>
2020-04-18 14:49:29 -0400

    tiled maps!

---
+++ Assets/GenerateGround.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GenerateGround : MonoBehaviour
+{
+    [SerializeField]
+    public Tile layableTile;
+    [SerializeField]
+    public Tilemap theMap;
+
+    int xPos = 0;
+
+    void Start()
+    {
+        StartCoroutine(groundBuild());
+    }
+
+    IEnumerator groundBuild()
+    {
+        while(true) 
+         { 
+            Vector3Int currentCell = theMap.WorldToCell(new Vector3(0 + ++xPos, -2.2f, 0f));
+            theMap.SetTile(currentCell, layableTile);
+            yield return new WaitForSeconds(0.1f);
+         }
+     }
+}
